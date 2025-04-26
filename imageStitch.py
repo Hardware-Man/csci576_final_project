962ccef2f1ef4516200b04b5ac5888b8a98b4d80
hoangann23 <annluong146@gmail.com>
2025-04-26 15:53:32 -0700

    displaying the first frame

--- imageStitch.py
+++ imageStitch.py
@@ -1,8 +1,18 @@
 # imageStitch.py
 
+import numpy as np
+import cv2
+import glob
+import imutils
+
 def stitchImage(folder):
-	print("TODO: IMPLEMENT IMAGE STITCHING")
-	pass
+	image_paths = glob.glob(folder + '*.jpg')
+	images = []
+	for image in image_paths:
+		img = cv2.imread(image)
+		images.append(img)
+	cv2.imshow("image", images[0])
+	cv2.waitKey(0)
 
 def processFrames(folders):
 	for folder in folders:
