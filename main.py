67f777c8af2f672997862d1ce594a4f6f3a75a7d
Ann <annluong146@gmail.com>
2025-04-26 00:29:24 -0700

    Extracting all frames from video

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import numpy as np
 import cv2
+import os
 
 video_name = 'video_data/video2/real_002.mp4'
-cap = cv2.VideoCapture(video_name)
 
-success, image = cap.read()
+def loadFrames(video_path):
+    try:
+        if not os.path.exists('out/extracted_frames'):
+            os.makedirs('out/extracted_frames')
+    except OSError:
+        print("Error: Creating output directory")
 
-if success:
-    cv2.imwrite("frame0.jpg", image)
-    print("Successfully captured first frame from '" + video_name + "' into 'frame0.jpg'")
-else:
-    print("Could not read '" + video_name + "'")
+    cap = cv2.VideoCapture(video_name)
+
+    count = 0
+
+    success = 1
+
+    while success:
+
+        success, image = cap.read()
+
+        if success:
+            cv2.imwrite("out/extracted_frames/frame%d.jpg" % count, image)
+            print(f"Successfully captured frame {count}")
+
+        count += 1
+
+if __name__ == "__main__":
+    loadFrames(video_name)
