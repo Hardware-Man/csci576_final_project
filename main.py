5cd71406ecd859d827f0d0dcca8fa8686d6ced61
Ann <annluong146@gmail.com>
2025-04-26 00:53:39 -0700

    adding command line arguments + support for dataset 5(multiple videos)

--- main.py
+++ main.py
@@ -1,31 +1,62 @@
 import numpy as np
 import cv2
 import os
+import sys
 
-video_name = 'video_data/video2/real_002.mp4'
+def getVideoPaths():
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <video number>")
+        sys.exit(1)
+    
+    num = int(sys.argv[1])
 
-def loadFrames(video_path):
-    try:
-        if not os.path.exists('out/extracted_frames'):
-            os.makedirs('out/extracted_frames')
-    except OSError:
-        print("Error: Creating output directory")
+    if num not in range(1,6):
+        print("Usage: video number must be between 1 and 5")
+        sys.exit(1)
 
-    cap = cv2.VideoCapture(video_name)
+    videoPaths = []
+    directory = f"video_data/video{num}/"
+    if num != 5:
+        videoPath = directory + f"real_00{num}.mp4"
+        videoPaths.append(videoPath)
+        print(f"Queuing {videoPath} for processing")
+        return videoPaths
+    else:
+        forest1 = directory + "forest1.mp4"
+        forest2 = directory + "forest2.mp4"
+        videoPaths.append(forest1)
+        print(f"Queuing {forest1} for processing")
+        videoPaths.append(forest2)
+        print(f"Queuing {forest2} for processing")
+        return videoPaths
+        
 
-    count = 0
+def loadFrames(videoPaths):
+    numVideos = len(videoPaths)
+    for i in range(numVideos):
+        outputDirectory = f"out/video{i+1}/extracted_frames/"
+        try:
+            if not os.path.exists(outputDirectory):
+                os.makedirs(outputDirectory)
+        except OSError:
+            print("Error: Creating output directory")
 
-    success = 1
+        video_name = videoPaths[i]
+        cap = cv2.VideoCapture(video_name)
 
-    while success:
+        count = 0
 
-        success, image = cap.read()
+        success = 1
 
-        if success:
-            cv2.imwrite("out/extracted_frames/frame%d.jpg" % count, image)
-            print(f"Successfully captured frame {count}")
+        while success:
+            success, image = cap.read()
 
-        count += 1
+            if success:
+                cv2.imwrite(outputDirectory + "frame%d.jpg" % count, image)
+                print(f"Successfully captured frame {count}")
+
+            count += 1
 
 if __name__ == "__main__":
-    loadFrames(video_name)
+    videoPaths = getVideoPaths()
+    loadFrames(videoPaths)
