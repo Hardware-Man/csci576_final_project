2179ab1a19f5b21df7026b1ea4531c4b76e40efb
Hardware-Man <kmishra686@gmail.com>
2025-05-02 01:30:37 -0700

    testing out stitching module + updates to stitchImages func

--- test.py
+++ test.py
@@ -1,120 +1,108 @@
 import numpy as np
 import cv2
-import sys
-from scipy.ndimage import rotate
 import matplotlib.pyplot as plt
+from stitching import Stitcher, AffineStitcher
+settings = {
+    'detector': 'orb',
+    'crop': False,
+    'confidence_threshold': 0.2,
+    'warper_type': 'plane',
+    'compensator': 'no',
+    'blender_type': "no",
+}  
+# stitcher = AffineStitcher(**settings)
+stitcher = Stitcher(**settings)
 
-video_name = 'video_data/video3/deer1.mp4'
-cap = cv2.VideoCapture(video_name)
-frame_number = 0
-cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number - 1)
-success, image1 = cap.read()
+image1 = cv2.imread("out/dataset3/video1/extracted_frames/frame000.jpg")
+image2 = cv2.imread("out/dataset3/video1/extracted_frames/frame015.jpg")
+# image1 = cv2.imread("result.png")
+# image2 = cv2.imread("out/dataset3/video1/extracted_frames/frame060.jpg")
 
-if not success:
-    sys.exit("Error in reading frame %d" %frame_number)
-else:
-    # image1 = rotate(image1, 90)
-    # cv2.imshow("Frame %d" %frame_number, image1)
-    print("Captured frame %d" %frame_number)
-    # cv2.waitKey(0)
+feature_extractor = cv2.ORB.create(nfeatures=1500, fastThreshold=3)
 
-frame_number = 30
-cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number - 1)
-success, image2 = cap.read()
+def averageTuple(tupleList):
+    avgX, avgY = 0,0
+    for tuple in tupleList:
+        avgX += tuple[0]
+        avgY += tuple[1]
+    return (int(avgX/len(tupleList)),int(avgY/len(tupleList)))
 
-if not success:
-    sys.exit("Error in reading frame %d" %frame_number)
-else:
-    # image2 = rotate(image2, 90)
-    # cv2.imshow("Frame %d" %frame_number, image2)
-    print("Captured frame %d" %frame_number)
-    # cv2.waitKey(0)
+def tupleInRange(t1, t2, dif=3):
+    if t1[0] + dif > t2[0] and t1[0] - dif < t2[0]:
+        if t1[1] + dif > t2[1] and t1[1] - dif < t2[1]:
+            return True
+    return False
+
+def cropExtra(img,extraRange=0.0):
+    y, x = img[:, :, 2].nonzero()
+    minx = int(np.min(x)*(1-extraRange))
+    miny = int(np.min(y)*(1-extraRange))
+    maxx = int(np.max(x)*(1+extraRange))
+    maxy = int(np.max(y)*(1+extraRange))
+    return img[miny:maxy, minx:maxx]
 
 def stitchImages(img1, img2):
-    # Initialize SIFT detector
-    sift = cv2.SIFT_create()
+    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-    # Detect keypoints and descriptors
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
+    kp1, des1 = feature_extractor.detectAndCompute(gray1,None)
+    kp2, des2 = feature_extractor.detectAndCompute(gray2,None)
 
-    # Use BFMatcher to find matches
-    bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
-    matches = bf.match(des1, des2)
+    # img1_kp = cv2.drawKeypoints(img1, kp1, None, color=(0,255,0), flags=0)
+    # img2_kp = cv2.drawKeypoints(img2, kp2, None, color=(0,255,0), flags=0)
 
-    # Sort matches by distance
-    matches = sorted(matches, key=lambda x: x.distance)
+    # plt.imshow(cv2.cvtColor(img1_kp, cv2.COLOR_BGR2RGB))
+    # plt.show()
+    # plt.imshow(cv2.cvtColor(img2_kp, cv2.COLOR_BGR2RGB))
+    # plt.show()
 
-    src_pts = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1,1,2)
-    dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1,1,2)
+    # # FlannBasedMatcher
+    # FLANN_INDEX_KDTREE = 1
+    # index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    # search_params = dict(checks = 50)
+    # matcher = cv2.FlannBasedMatcher(index_params, search_params)
+    
+    # Brute Force Matcher
+    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    
+    matches = matcher.match(des1, des2)
+    matches = sorted(matches, key = lambda x:x.distance)
+    good_matches = matches[:10]
 
-    start = (abs(int(dst_pts[0][0][0]-src_pts[0][0][0])), abs(int(dst_pts[0][0][1]-src_pts[0][0][1])))
+    # matches = matcher.knnMatch(des1, des2, k=2)
+    # good_matches = []
+    # for m,n in matches:
+    #     if m.distance < 0.5*n.distance:
+    #         good_matches.append(m)
+    
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1,1,2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1,1,2)
+
+    pointsList = []
+    for index in range(0,len(src_pts)):
+        curPoint = (int(dst_pts[index][0][0]-src_pts[index][0][0])), (int(dst_pts[index][0][1]-src_pts[index][0][1]))
+        pointsList.append(curPoint)
+
+    start = pointsList[0]
+    avgTuple = averageTuple(pointsList)
+    if not tupleInRange(start, avgTuple): return img1
+
     h1, w1 = img1.shape[:2]
     h2, w2 = img2.shape[:2]
-    vis = np.zeros((start[1]+h1,start[0]+w1,3), np.uint8)
-    vis[start[1]:start[1]+h1, start[0]:start[0]+w1, :3] = img1
-    vis[:h2, :w2, :3] = img2
-    return vis
+    ax = abs(start[0])
+    ay = abs(start[1])
+    result = np.zeros((ay+h1,ax+w1,3), np.uint8)
 
-# use orb if sift is not installed
-feature_extractor = cv2.SIFT_create()
+    ofst2 = (ax if start[0]<0 else 0, ay if start[1]<0 else 0)
+    ofst1 = (0 if start[0]<0 else ax, 0 if start[1]<0 else ay)
+    result[ofst1[1]:ofst1[1]+h1, ofst1[0]:ofst1[0]+w1, :3] = img1
+    result[ofst2[1]:ofst2[1]+h2, ofst2[0]:ofst2[0]+w2, :3] = img2
+    return cropExtra(result,0.001)
 
-# find the keypoints and descriptors with chosen feature_extractor
-kp1, desc1 = feature_extractor.detectAndCompute(image1, None)
-kp2, desc2 = feature_extractor.detectAndCompute(image2, None)
-
-bf = cv2.BFMatcher()
-matches = bf.knnMatch(desc1, desc2, k=2)
-
-# Apply ratio test
-good_and_second_good_match_list = []
-for m in matches:
-    if m[0].distance / m[1].distance < 0.5:
-        good_and_second_good_match_list.append(m)
-good_match_arr = np.asarray(good_and_second_good_match_list)[:, 0]
-
-# show only 30 matches
-im_matches = cv2.drawMatchesKnn(
-    image1,
-    kp1,
-    image2,
-    kp2,
-    good_and_second_good_match_list[0:30],
-    None,
-    flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
-)
-
-# plt.figure(figsize=(20, 20))
-# plt.imshow(im_matches)
-# plt.title("keypoints matches")
-# plt.show()
-
-good_kp_l = np.array([kp1[m.queryIdx].pt for m in good_match_arr])
-good_kp_r = np.array([kp2[m.trainIdx].pt for m in good_match_arr])
-H, masked = cv2.findHomography(good_kp_r, good_kp_l, cv2.RANSAC, 5.0)
-
-def warpTwoImages(img1, img2, H):
-    """warp img2 to img1 with homograph H
-    from: https://stackoverflow.com/questions/13063201/how-to-show-the-whole-image-when-using-opencv-warpperspective
-    """
-    h1, w1 = img1.shape[:2]
-    h2, w2 = img2.shape[:2]
-    pts1 = np.float32([[0, 0], [0, h1], [w1, h1], [w1, 0]]).reshape(-1, 1, 2)
-    pts2 = np.float32([[0, 0], [0, h2], [w2, h2], [w2, 0]]).reshape(-1, 1, 2)
-    pts2_ = cv2.perspectiveTransform(pts2, H)
-    pts = np.concatenate((pts1, pts2_), axis=0)
-    [xmin, ymin] = np.int32(pts.min(axis=0).ravel() - 0.5)
-    [xmax, ymax] = np.int32(pts.max(axis=0).ravel() + 0.5)
-    t = [-xmin, -ymin]
-    Ht = np.array([[1, 0, t[0]], [0, 1, t[1]], [0, 0, 1]])  # translate
-
-    result = cv2.warpPerspective(img2, Ht @ H, (xmax - xmin, ymax - ymin))
-    result[t[1] : h1 + t[1], t[0] : w1 + t[0]] = img1
-    return result
-
-result = warpTwoImages(image1, image2, H)
-# result = stitchImages(image2, image1)
+result = stitchImages(image1, image2)
+# result = stitcher.stitch([image1, image2])
 plt.figure(figsize=(10, 10))
-plt.imshow(result)
-plt.title("better warping")
+plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
 plt.show()
+
+cv2.imwrite("result.png", result)
